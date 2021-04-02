806f969026c2bf57888568b7a2ade8b789ec45cf
mgfjx <mgfjxxiexiaolong@gmail.com>
2021-04-02 18:43:58 +0800

    fix

--- 油猴美化脚本.js
+++ 油猴美化脚本.js
@@ -6,8 +6,9 @@
 // @author       You
 // @include      *://*.zhihu.com/*
 // @include      https://www.baidu.com/
-// @include      *://www.jianshu.com/go-wild?*
+// @include      *://www.jianshu.com/*
 // @include      *://blog.csdn.net/*
+// @include      *://www.pgyer.com/*
 // @match
 // @require      http://code.jquery.com/jquery-1.11.0.min.js
 // @grant        GM_addStyle
@@ -30,11 +31,18 @@
   //百度首页
   if (window.location.host == "www.baidu.com") {
     console.log("百度油猴...");
-    document.getElementById("s_mp").remove();
+
+    try {
+      $("#s_mp").remove();
+      $(".user-name").remove();
+    } catch {
+      console.log("百度油猴出现异常!");
+    }
+
     GM_addStyle(`
-      #s-top-left, #u1, #s_side_wrapper, #bottom_layer, .s-menu-container,
+      #s-top-left, .s-weather-wrapper, #s_side_wrapper, #bottom_layer,
       .ipt_rec, .c-color-text, .s-mblock-title .s-opacity-border4-bottom,
-      .s-top-nav, .tips-manager-area {
+      .s-top-nav, .tips-manager-area, #s-hotsearch-wrapper, #s_main, .soutu-btn {
           display: none !important;
       }
       .s-block-container, #s_main {
@@ -44,7 +52,10 @@
           background: rgba(0,0,0,0);
       }
       .s-skin-hasbg #s_main {
-          background: rgba(0,0,0,.2);
+          background: rgba(0,0,0,0);
+      }
+      .s-code-blocks {
+        box-shadow: none !important;
       }
       .nav-text {
         color: rgba(200,200,200,1);
@@ -55,6 +66,22 @@
       .s-content {
         padding-bottom: 0px;
       }
+      #head_wrapper .s_btn {
+        background-color: rgba(0,0,0,1) !important;
+      }
+
+      #head_wrapper .s_btn:hover {
+        background-color: rgb(29 27 27) !important;
+      }
+
+      #head_wrapper #kw {
+        border-color: #000000 !important;
+      }
+
+      #head_wrapper #kw:focus {
+        border-color: #000000 !important;
+      }
+
     `);
   }
 
@@ -71,12 +98,16 @@
         if (count == 0) {
           clearInterval(time);
           var url = $("._2VEbEOHfDtVWiQAJxSIrVi_0").val();
-          window.location.href = url;
+          if (url) {
+            window.location.href = url;
+          }
         }
       }, 1000);
     })();
     GM_addStyle(`
-
+      ._1F7CTF, ._13lIbp, ._1jKNin {
+        display: none !important;
+      }
     `);
   }
 
@@ -85,6 +116,16 @@
     console.log("csdn油猴...");
     GM_addStyle(`
         .toolbar-advert {
+          display: none !important;
+        }
+      `);
+  }
+
+  //蒲公英
+  if (window.location.host == "www.pgyer.com") {
+    console.log("蒲公英油猴...");
+    GM_addStyle(`
+        .ad-container {
           display: none !important;
         }
       `);

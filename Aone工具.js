dfb872ff53326e05bd67505b0fe6be7d40b5d820
xiexiaolong.xxl <xiexiaolong.xxl@alibaba-inc.com>
2022-05-06 09:53:50 +0800

    Aone update

--- Aone工具.js
+++ Aone工具.js
@@ -17,29 +17,30 @@
   'use strict';
   console.log("Aone油猴...");
 
+  //弹出提示框
   function addIndicator(title) {
     $("body").append('<div class="xxl_container"><span>' + title + '</span></div>');
-      let ele = $(".xxl_container");
-      $(ele).css({
-        backgroundColor: "antiquewhite",
-        position: "absolute",
-        top: "200px",
-        right: "-150px",
-        width: "150px",
-        textAlign: "center",
-        padding: "10px",
-        borderRadius: "10px",
-        color: "rgb(27, 105, 79)",
-        opacity: "0",
+    let ele = $(".xxl_container");
+    $(ele).css({
+      backgroundColor: "antiquewhite",
+      position: "absolute",
+      top: "150px",
+      left: "-150px",
+      width: "150px",
+      textAlign: "center",
+      padding: "10px",
+      borderRadius: "10px",
+      color: "rgb(27, 105, 79)",
+      opacity: "0",
+    });
+    let display = $(ele).css("opacity");
+    $(ele).fadeTo(250, 1.0);
+    $(ele).animate({ left: "+=150px" }, 250);
+    setTimeout(() => {
+      $(ele).fadeTo(250, 0, function () {
+        $(ele).remove();
       });
-      let display = $(ele).css("opacity");
-      $(ele).fadeTo(250, 1.0);
-      $(ele).animate({right: "+=150px"}, 250);
-      setTimeout(() => {
-        $(ele).fadeTo(250, 0, function() {
-          $(ele).remove();
-        });
-      }, 1250);
+    }, 1250);
   }
 
   setTimeout(() => {
@@ -47,17 +48,25 @@
     let parent = $('.detail-content .next-card-title div').parent();
     let title = $('.detail-content .next-card-title div span').text();
     $('.detail-content  .next-card-title div').remove();
-    $(parent).html('<span class="xxl_btn detail-title" style="word-break: break-all;"></span>');
+    $(parent).html('<span class="xxl_btn detail-title" title="单击复制标题" style="word-break: break-all;"></span>');
     $('.xxl_btn').text(title);
     $(".xxl_btn").css({
       fontSize: 24
     });
-    $(".xxl_btn").on("click", function () {
-      console.log("你好");
+    $(".xxl_btn").on("click", function (e) {
+      console.log("x: " + e.pageX + ", y: " + e.pageY);
       let title = $('.xxl_btn').text();
       addIndicator("您已复制标题内容!");
       GM_setClipboard(title);
     });
+    $(".detail-basic-id").on("click", function (e) {
+      console.log("x: " + e.pageX + ", y: " + e.pageY);
+      let title = $('.detail-basic-id').text();
+      addIndicator("您已复制BugId !");
+      GM_setClipboard(title);
+    });
+
+    //获取当前鼠标位置
 
   }, 200);
 
@@ -100,10 +109,53 @@
     }
   `);
 
+  window.staticCount = 0;
+
+  function modifyActiveStyle() {
+    console.log("count: ", window.staticCount);
+    let cycleTime = 200;
+    setTimeout(() => {
+      if (window.staticCount * cycleTime > 5000) {
+        window.staticCount = 0;
+        return;
+      }
+      window.staticCount++;
+      let items = $('.nav-tabs .group-item a');
+      for (let i = 0; i < items.length; i++) {
+        const ele = items[i];
+        $(ele).attr("style", "border:2px solid #4b8bf4;");
+      }
+      let li = $('.nav-tabs .active');
+      if (li.length == 0) {
+        console.log("无active选中态!");
+        modifyActiveStyle();
+        return;
+      }
+      let active = $('.nav-tabs .active a');
+      $(active).attr("style", "border:2px solid #ff0000;");
+      console.log("li: " + li);
+      if (window.staticCount < 20) {
+        window.staticCount = 20; //状态维持1秒钟
+      }
+      modifyActiveStyle();
+    }, cycleTime);
+  }
+
+  window.addEventListener("hashchange", myFunction);
+  function myFunction() {
+    let url = window.location.href;
+    let reg = new RegExp("https://aone.alibaba-inc.com/project/\\d+/issue\\?#filter");
+    if (url.match(reg)) {
+      modifyActiveStyle();
+    }
+  }
+
+  modifyActiveStyle();
+
   //bug筛选
   GM_addStyle(`
     .group-item>a{
-      border:2px solid #4b8bf4 !important;
+      border:2px solid #4b8bf4;
       padding: 5px 30px 5px 10px;
       border-radius: 8px !important;
     }
